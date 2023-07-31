d121c48261c15a1d36385cec79a6cd64eaa8873f
Andrew Lall <andrew.s.lall@gmail.com>
2023-07-31 16:34:42 -0400

    last push for 7/31/23

--- EmployeesHrSolution/EmployeesHrApi/Controllers/EmployeesController.cs
+++ EmployeesHrSolution/EmployeesHrApi/Controllers/EmployeesController.cs
@@ -1,30 +1,44 @@
-﻿
-
-using EmployeesHrApi.Data;
+﻿using EmployeesHrApi.Data;
+using EmployeesHrApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace EmployeesHrApi.Controllers
+
+namespace EmployeesHrApi.Controllers;
+
+public class EmployeesController : ControllerBase
 {
-    public class EmployeesController : ControllerBase
+
+    private readonly EmployeeDataContext _context;
+
+    public EmployeesController(EmployeeDataContext context)
     {
-        private readonly EmployeeDataContext _context;
-
-        public EmployeesController(EmployeeDataContext context)
-        {
-            _context = context;
-        }
-
-
-
-
-        //GET /employees
-        [HttpGet("/employees")]
-        public async Task<ActionResult> GetEmployeesAsync()
-        {
-            var employees = await _context.Employees.ToListAsync();
-
-            return Ok(employees);
-        }
+        _context = context;
     }
+
+
+
+    // GET /employees
+    [HttpGet("/employees")]
+    public async Task<ActionResult> GetEmployeesAsync()
+    {
+        var employees = await _context.Employees
+            .Select(emp => new EmployeesSummaryResponseModel
+            {
+                Id = emp.Id.ToString(),
+                FirstName = emp.FirstName,
+                LastName = emp.LastName,
+                Department = emp.Department,
+                Email = emp.Email,
+            })
+            .ToListAsync();
+        // TODO: Get Back to This
+
+        var response = new EmployeesResponseModel
+        {
+            Employees = employees
+        };
+        return Ok(response);
+    }
+
 }
